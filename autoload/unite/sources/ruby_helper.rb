14c87426be963a968e54b441e072c9478e80e46a
ujihisa <ujihisa@gmail.com>
2013-06-19 22:56:23 -0700

    why select?

--- autoload/unite/sources/ruby_helper.rb
+++ autoload/unite/sources/ruby_helper.rb
@@ -5,7 +5,7 @@ b =
   rescue
     []
   end
-puts $LOAD_PATH.select {|l| l=~/ruby\/\d\.\d\.\d$/ }.
+puts $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
   map {|l| Dir.glob(l+"/**/*").map{|p| p=~/#{l}\/(.+)\.rb$/; $1}}.
   flatten.compact.sort +
   (b + Gem::default_path).map {|p| Dir.glob(p+"/**/*.rb").map{|g| g=~/#{p}\/.+\/lib\/(.+)\.rb$/; $1 }}.
