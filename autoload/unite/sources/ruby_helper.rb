ee83214e3fb567f34ae71608b66ccf8de5a1b5a2
ujihisa <ujihisa@gmail.com>
2013-06-19 23:06:11 -0700

    be more human readable

--- autoload/unite/sources/ruby_helper.rb
+++ autoload/unite/sources/ruby_helper.rb
@@ -5,8 +5,10 @@ b =
   rescue
     []
   end
-puts $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
-  map {|l| Dir.glob("#{l}/**/*.rb").map{|p| p[/#{l}\/(.+)\.rb$/, 1] }}.
-  flatten.compact.sort +
-  (b + Gem.default_path).map {|p| Dir.glob("#{p}/**/*.rb").map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }}.
+stdlibs = $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
+  map {|l| Dir.glob("#{l}/**/*.rb").map {|p| p[/#{l}\/(.+)\.rb$/, 1] }}.
+  flatten.compact.sort
+gemlibs = (b + Gem.default_path).
+  map {|p| Dir.glob("#{p}/**/*.rb").map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }}.
   flatten.compact.sort.uniq
+puts stdlibs + gemlibs
