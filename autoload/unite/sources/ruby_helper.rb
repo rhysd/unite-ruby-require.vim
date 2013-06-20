f859989e9d8be5ab2a2a1e0c8fd082deb7e79dcf
ujihisa <ujihisa@gmail.com>
2013-06-19 22:54:26 -0700

    why destructive?

--- autoload/unite/sources/ruby_helper.rb
+++ autoload/unite/sources/ruby_helper.rb
@@ -3,9 +3,9 @@ begin
   b=[Bundler::bundle_path.to_s]
 rescue
   b=[]
-end;
+end
 puts $LOAD_PATH.select {|l| l=~/ruby\/\d\.\d\.\d$/ }.
   map {|l| Dir.glob(l+"/**/*").map{|p| p=~/#{l}\/(.+)\.rb$/; $1}}.
-  flatten!.compact!.sort! +
+  flatten.compact.sort +
   (b+Gem::default_path).map{|p| Dir.glob(p+"/**/*.rb").map{|g| g=~/#{p}\/.+\/lib\/(.+)\.rb$/; $1 }}.
-  flatten!.compact!.sort!.uniq!
+  flatten.compact.sort.uniq
