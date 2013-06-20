997304612660294d926e455ee12723faf9682adc
ujihisa <ujihisa@gmail.com>
2013-06-19 23:19:50 -0700

    cosmetic change + debug info

--- autoload/unite/sources/ruby_helper.rb
+++ autoload/unite/sources/ruby_helper.rb
@@ -1,8 +1,15 @@
 require "bundler"
 stdlibs = $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
-  map {|l| Dir.glob("#{l}/**/*.rb").map {|p| p[/#{l}\/(.+)\.rb$/, 1] }}.
+  map {|l|
+    Dir.glob("#{l}/**/*.rb").
+      map {|p| p[/#{l}\/(.+)\.rb$/, 1] }
+  }.
   flatten.compact.sort
 gemlibs = (([Bundler.bundle_path.to_s] rescue []) + Gem.default_path).
-  map {|p| Dir.glob("#{p}/**/lib/**/*.rb").map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }}.
+  map {|p|
+    Dir.glob("#{p}/**/lib/**/*.rb").
+      map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }
+  }.
   flatten.compact.sort.uniq
-puts stdlibs + gemlibs
+#puts stdlibs + gemlibs
+p [stdlibs.size, gemlibs.size]
