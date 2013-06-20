0fba1973e4c3b015fa62d71a7ea81b98d62109fe
ujihisa <ujihisa@gmail.com>
2013-06-19 23:28:50 -0700

    Revert "use thread"
    This reverts commit 86ba63219fab11d6794476dc393738f73496775c.

--- autoload/unite/sources/ruby_helper.rb
+++ autoload/unite/sources/ruby_helper.rb
@@ -6,14 +6,10 @@ stdlibs = $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
   }.
   flatten.compact.sort
 gemlibs = (([Bundler.bundle_path.to_s] rescue []) + Gem.default_path).
-  uniq.
   map {|p|
-    Thread.start {
-      Dir.glob("#{p}/**/lib/**/*.rb").
-        map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }
-    }
+    Dir.glob("#{p}/**/lib/**/*.rb").
+      map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }
   }.
-  map(&:value).
   flatten.compact.sort.uniq
 #puts stdlibs + gemlibs
 p [stdlibs.size, gemlibs.size]
