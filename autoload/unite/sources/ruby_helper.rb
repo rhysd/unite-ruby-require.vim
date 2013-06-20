8a7463bdef9a0945f4326143e5a7160e1b1dc6ff
ujihisa <ujihisa@gmail.com>
2013-06-19 23:08:11 -0700

    narrow down in advance

--- autoload/unite/sources/ruby_helper.rb
+++ autoload/unite/sources/ruby_helper.rb
@@ -3,6 +3,6 @@ stdlibs = $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
   map {|l| Dir.glob("#{l}/**/*.rb").map {|p| p[/#{l}\/(.+)\.rb$/, 1] }}.
   flatten.compact.sort
 gemlibs = (([Bundler.bundle_path.to_s] rescue []) + Gem.default_path).
-  map {|p| Dir.glob("#{p}/**/*.rb").map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }}.
+  map {|p| Dir.glob("#{p}/**/lib/**/*.rb").map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }}.
   flatten.compact.sort.uniq
 puts stdlibs + gemlibs
