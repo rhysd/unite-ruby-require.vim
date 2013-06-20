5ebc0fc336ca0237e89ed3d05c3b85d74eb41d66
ujihisa <ujihisa@gmail.com>
2013-06-19 23:33:22 -0700

    oops bugfix: require had to be inside rescue

--- autoload/unite/sources/ruby_helper.rb
+++ autoload/unite/sources/ruby_helper.rb
@@ -1,14 +1,14 @@
-require "bundler"
 stdlibs = $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
   map {|l|
     Dir.glob("#{l}/**/*.rb").
       map {|p| p[/#{l}\/(.+)\.rb$/, 1] }
   }.
   flatten.compact.sort
-gemlibs = (([Bundler.bundle_path.to_s] rescue []) + Gem.default_path).
+bundler_paths = [(require "bundler"; Bundler.bundle_path.to_s)] rescue []
+gemlibs = (bundler_paths + Gem.default_path).
   map {|p|
     Dir.glob("#{p}/**/lib/**/*.rb").
-      map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }
+      map {|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }
   }.
   flatten.compact.sort.uniq
 #puts stdlibs + gemlibs
