0697ce5b339b710fed79f7bf167690401c2f1f5d
ujihisa <ujihisa@gmail.com>
2013-06-20 09:35:19 -0700

    don't sleep

--- autoload/unite/sources/ruby_helper.rb
+++ autoload/unite/sources/ruby_helper.rb
@@ -1,4 +1,3 @@
-sleep 1
 stdlibs = $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
   map {|l|
     r = /^#{l}\/(.+)\.rb$/
@@ -7,7 +6,6 @@ stdlibs = $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
   flatten.sort
 #bundler_paths = [(require 'bundler'; Bundler.bundle_path.to_s)] rescue []
 puts stdlibs
-sleep 2
 bundler_paths = []
 gemlibs = (bundler_paths + Gem.default_path).
   compact.flatten.
