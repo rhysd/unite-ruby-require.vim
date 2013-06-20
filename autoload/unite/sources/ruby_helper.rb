8366eec0ea7a30bfbd3b6dd80f47500428ca1cc6
ujihisa <ujihisa@gmail.com>
2013-06-20 01:54:37 -0700

    trivial performance improvement by reducing number of elements in arrays

--- autoload/unite/sources/ruby_helper.rb
+++ autoload/unite/sources/ruby_helper.rb
@@ -1,16 +1,16 @@
 stdlibs = $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
   map {|l|
     r = /^#{l}\/(.+)\.rb$/
-    Dir.glob("#{l}/**/*.rb").map {|p| p[r, 1] }
+    Dir.glob("#{l}/**/*.rb").map {|p| p[r, 1] }.compact
   }.
-  flatten.compact.sort
+  flatten.sort
 bundler_paths = [(require 'bundler'; Bundler.bundle_path.to_s)] rescue []
 gemlibs = (bundler_paths + Gem.default_path).
   flatten.
   map {|p|
     r = /^#{p}\/.+\/lib\/(.+)\.rb$/
-    Dir.glob("#{p}/**/lib/**/*.rb").map {|g| g[r, 1] }
+    Dir.glob("#{p}/**/lib/**/*.rb").map {|g| g[r, 1] }.compact
   }.
-  flatten.compact.sort.uniq
-puts stdlibs + gemlibs
-#p [stdlibs.size, gemlibs.size]
+  flatten.sort.uniq
+#puts stdlibs + gemlibs
+p [stdlibs.size, gemlibs.size]
