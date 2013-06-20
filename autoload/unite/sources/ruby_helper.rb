53b4b6e5c973f10811809403a430dc6a178f274e
ujihisa <ujihisa@gmail.com>
2013-06-19 23:00:20 -0700

    Why didn't Dir::glob?

--- autoload/unite/sources/ruby_helper.rb
+++ autoload/unite/sources/ruby_helper.rb
@@ -1,12 +1,12 @@
 b =
   begin
     require "bundler"
-    [Bundler::bundle_path.to_s]
+    [Bundler.bundle_path.to_s]
   rescue
     []
   end
 puts $LOAD_PATH.grep(/ruby\/\d\.\d\.\d$/).
   map {|l| Dir.glob("#{l}/**/*").map{|p| p[/#{l}\/(.+)\.rb$/, 1] }}.
   flatten.compact.sort +
-  (b + Gem::default_path).map {|p| Dir.glob("#{p}/**/*.rb").map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }}.
+  (b + Gem.default_path).map {|p| Dir.glob("#{p}/**/*.rb").map{|g| g[/#{p}\/.+\/lib\/(.+)\.rb$/, 1] }}.
   flatten.compact.sort.uniq
